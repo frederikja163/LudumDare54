7bd20efd20b345764b55b195377936f751f18581
FrederikJA <fjaplv64@gmail.com>
2023-09-30 02:30:20 +0200

    Include p5js into the project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-function main(){
-    console.log("Testing!");
+import P5 from "p5";
+
+function sketch(p5: P5){
+    p5.setup = () => {
+        p5.createCanvas(400, 400);
+    }
+    
+    p5.draw = () => {
+        p5.background(0, 255, 0);
+    }
 }
 
-main();
+new P5(sketch);
