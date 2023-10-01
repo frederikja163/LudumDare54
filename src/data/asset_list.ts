25eea53869928e175334ee22a35fe3d8ce0d6912
FrederikJA <fjaplv64@gmail.com>
2023-10-01 02:40:37 +0200

    Add new icons

--- src/data/asset_list.ts
+++ src/data/asset_list.ts
@@ -2,7 +2,7 @@ import P5 from "p5";
 
 export class AssetList{
     public readonly dirt: P5.Image[] = [];
-    public readonly dirtPaths: string[] = [getPath("dirt/dirt1.png"), getPath("dirt/dirt2.png")];
+    public readonly dirtPaths: string[] = [getPath("dirt/dirt1.png"), getPath("dirt/dirt2.png"), getPath("dirt/dirt3.png"), getPath("dirt/dirt4.png")];
 
     public readonly testPath: string = getPath("test.png");
 }
