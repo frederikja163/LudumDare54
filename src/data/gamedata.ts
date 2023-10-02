cc70ee6c82a7b5dacdec42e0e56f91723b677912
OliverEC <oliverec04@gmail.com>
2023-10-02 23:04:30 +0200

    bal2

--- src/data/gamedata.ts
+++ src/data/gamedata.ts
@@ -12,6 +12,7 @@ export class GameData {
     public readonly residentialTiles = value(0);
     public readonly farmTiles = value(0);
     public readonly trainingTiles = value(0);
+    public readonly totalTiles = value(0);
 
     public readonly queensTotal = value(1);
     public readonly farmersTotal = value(2);
@@ -19,7 +20,7 @@ export class GameData {
     public readonly soldiersTotal = value(1);
 
     public readonly queensActive = this.queensTotal;
-    public readonly workersActive = value(0);
+    public readonly workersActive = equation([this.totalTiles, this.queensTotal], n => (n[0] - 30 * n[1]));
     public readonly soldiersActive = value(0);
 
     public readonly queensIdle = equation([this.queensTotal, this.queensActive], n => n[0] - n[1]);
@@ -28,7 +29,7 @@ export class GameData {
 
     // Farmers cant have more than 1 consumption atm as this would create a circular dependency. Luckily we planned for a consumption of 1.
     public readonly foodConsumption = weightedSum([this.queensActive, this.farmersTotal, this.workersActive, this.soldiersActive, this.queensIdle, this.workersIdle, this.soldiersIdle],
-        [10, 1, 3, 2, 1, 1, 1]);
+        [8, 1, 3, 2, 1, 1, 1]);
     public readonly farmersActive = equation([this.farmerProduction, this.farmersTotal, this.farmProduction, this.farmTiles, this.foodConsumption],
         // Min(productionBaseOnFarmers, productionBasedOnFarms)
         n => Math.min(Math.min(Math.ceil(n[4] / n[0]), n[1]), Math.ceil(n[3] * n[0] / n[2])));
@@ -38,7 +39,6 @@ export class GameData {
     public readonly antsIdle = sum([this.queensIdle, this.farmersIdle, this.workersIdle, this.soldiersIdle]);
     public readonly antsActive = sum([this.queensActive, this.farmersActive, this.workersActive, this.soldiersActive]);
     public readonly antsTotal = sum([this.antsIdle, this.antsActive]);
-    public readonly totalTiles = value(0);
 
     // ants/min
     public readonly antProduction = equation([this.queenTiles], n => n[0] < 21 ? 1 : Math.log(n[0] - 20) / Math.log(2));
@@ -46,7 +46,7 @@ export class GameData {
     public readonly antSpawnProgress = value(0);
     public readonly antCapacity = product([this.residentialTiles, value(3)]);
     public readonly combatPower = product([this.trainingTiles, this.soldiersActive]);
-    public readonly tileCapacity = sum([product([value(1), sum([this.workersActive, this.workersIdle])]), value(40)]);
+    public readonly tileCapacity = equation([this.workersTotal, this.queensTotal], n => (3 * n[0] + 30 * n[1]));
 
     public readonly farmerSpawnRatio = value(100);
     public readonly workerSpawnRatio = value(100);
